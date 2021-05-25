c1d4cca061d51e890cf08bbc505cb221c9796ac2
Allan Chua <allanchua.officefiles@gmail.com>
2021-05-25 14:49:52 +0800

    Update delete-all-containers.sh

--- scripts/000_installations/delete-all-containers.sh
+++ scripts/000_installations/delete-all-containers.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Stop and delete any docker containers inside the box
+# Stop and delete any docker containers
 if [ "$(docker ps -qa)" ]; then
- echo "I will stop and clear all docker containers on AMI box";
  docker stop $(docker ps -aq);
  docker rm $(docker ps -aq);
 fi
