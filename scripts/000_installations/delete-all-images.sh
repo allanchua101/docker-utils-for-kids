8ff86d9793f99448cb991b40fd8a361731a423e0
Allan Chua <allanchua.officefiles@gmail.com>
2021-05-25 14:50:07 +0800

    Update delete-all-images.sh

--- scripts/000_installations/delete-all-images.sh
+++ scripts/000_installations/delete-all-images.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Delete all docker images inside the box
 if [ "$(docker images -qa)" ]; then
-  echo "I will stop and clear all docker images on AMI box";
   docker rmi $(docker images -qa);
 fi
